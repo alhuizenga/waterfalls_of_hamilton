517108f9bf5210b03d0e1bcc74d35fba2a77a318
alhuizenga <al.huizenga@gmail.com>
2017-05-02 21:57:31 -0400

    Add files via upload

---
+++ js/waterfalls.js
@@ -0,0 +1,162 @@
+var waterfalls = [
+	{
+	name: "Albion Falls",
+	position: {lat:43.2004722, lng: -79.8196778},
+	type: "Cascade",
+	height: 19,
+	width: 18,
+	watercourse: "Red Hill Creek"
+	},
+	{
+	name: "Ancaster Heights Falls",
+	position: {lat: 43.1424, lng: -79.5747},
+	type: "Terraced Ribbon Cascade",
+	height: 13.4,
+	width: 1.8,
+	watercourse: "Tiffany tributary"
+	},
+	{
+	name: "Borer's Falls",
+	position: {lat: 43.293976, lng: -79.936812},
+	type: "Ribbon",
+	height: 15,
+	width: 9,
+	watercourse: "Borer's Creek"
+	},
+	{
+	name: "Boundary Falls",
+	position: {lat: 43.3311, lng: -79.8881},
+	type: "Complex ribbon",
+	height: 6,
+	width: 2,
+	watercourse: "Grindstone Tributaries"
+	},
+	{
+	name: "Buttermilk Falls",
+	position: {lat: 43.203925, lng: -79.820480},
+	type: "Plunge",
+	height: 23,
+	width: 8,
+	watercourse: "Red Hill Creek"
+	},
+	{
+	name: "Canterbury Falls",
+	position: {lat: 43.233197, lng: -79.979782},
+	type: "Ribbon",
+	height: 9,
+	width: 4,
+	watercourse: "Canterbury Creek"
+	},
+	{
+	name: "Chedoke Falls",
+	position: {lat: 43.241576, lng: -79.900475},
+	type: "Ribbon",
+	height: 15.5,
+	width: 2,
+	watercourse: "Chedoke Creek"
+	},
+	{
+	name: "Cliffview Falls",
+	position: {lat: 43.244703, lng: -79.906483},
+	type: "Ribbon",
+	height: 15,
+	width: 3,
+	watercourse: "Chedoke tributary"
+	},
+	{
+	name: "Darnley Cascade",
+	position: {lat: 43.27648, lng: -80.00685},
+	type: "Cascade",
+	height: 4,
+	width: 22,
+	watercourse: "Spencer Creek"
+	},
+	{
+	name: "Devil's Punch Bowl",
+	position: {lat: 43.2104, lng: -79.7558},
+	type: "Ribbon waterfall",
+	height: 37,
+	width: 11,
+	watercourse: "Stoney Creek"
+	},
+	{
+	name: "Dundas Falls",
+	position: {lat: 43.272800, lng: -79.971886},
+	type: "Classical",
+	height: 8,
+	width: 8,
+	watercourse: "Spencer Creek"
+	},
+	{
+	name: "Hermitage Cascade",
+	position: {lat: 43.2363319111, lng: -79.9980491023},
+	type: "Cascade",
+	height: 4,
+	width: 5,
+	watercourse: "Hermitage Creek"
+	},
+	{
+	name: "Mineral Springs Falls",
+	position: {lat: 43.237700, lng: -80.019779},
+	type: "Cascade",
+	height: 3,
+	width: 2,
+	watercourse: "Sulphur Creek"
+	},
+	{
+	name: "Scenic Falls",
+	position: {lat: 43.244578, lng: -79.933691},
+	type: "Ribbon/Plunge",
+	height: 18,
+	width: 6,
+	watercourse: "Unnamed Creek"
+	},
+	{
+	name: "Sherman Falls",
+	position: {lat: 43.235699, lng: -79.974289},
+	type: "Ribbon",
+	height: 17,
+	width: 8,
+	watercourse: "Ancaster Creek"
+	},
+	{
+	name: "Tiffany Falls",
+	position: {lat: 43.1422, lng: -79.5731},
+	type: "Ribbon",
+	height: 21,
+	width: 6,
+	watercourse: "Tiffany Creek"
+	},
+	{
+	name: "Tews Falls",
+	position: {lat: 43.28082, lng: -79.9784},
+	type: "Plunge",
+	height: 41,
+	width: 9,
+	watercourse: "Logie's Creek"
+	},
+	{
+	name: "Vinemount East Falls",
+	position: {lat: 43.1211, lng: -79.4108},
+	type: "Horsetail, Ribbon, cascade",
+	height: 4,
+	width: 6,
+	watercourse: "Surface water"
+	},
+	{
+	name: "Washboard Falls",
+	position: {lat: 43.1418, lng: -79.5728},
+	type: "Cascade",
+	height: 5,
+	width: 6,
+	watercourse: "Tiffany Creek"
+	},
+	{
+	name: "Webster's Falls",
+	position: {lat: 43.276241, lng: -79.980898},
+	type: "Curtain/Plunge",
+	height: 22,
+	width: 30,
+	watercourse: "Spencer Creek"
+	}
+];
